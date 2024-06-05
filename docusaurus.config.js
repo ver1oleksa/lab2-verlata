166b46d19deb19c0120fada156f53a0d71b68236
Олекса Верлата <verlata@lll.kpi.ua>
2024-06-06 02:51:11 +0300

    Updated the config.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Lab Work 2',
-  tagline: 'Slava Ukrayini!',
+  tagline: 'Slava Ukrayini',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
